89e418926dac8d2d5aa2694cfef77f6ecb80dd95
Ajay Ratnam <ajratnam.dev@gmail.com>
2023-07-09 14:37:16 +0530

    Added typehints and docstrings and font customisation

--- converter.py
+++ converter.py
@@ -1,34 +1,118 @@
+from collections.abc import Sequence
+from typing import Optional
+
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from requests import get
 
 from config import CONVERSION_CHARACTERS
 
-font = ImageFont.truetype('monos.ttf', 20)
+base_font = ImageFont.truetype('monos.ttf', 20)
 
 
-def sizeof(text):
+def sizeof(text: str, font: ImageFont.ImageFont = base_font) -> tuple[int, int]:
+    """
+    Gets the size of the text when rendered in the given font in pixels
+
+    Parameters
+    ----------
+    text: str
+        The input text to render
+    font: ImageFont.ImageFont, optional, default: base_font
+        The font to use for the text size calculation (default: monos.ttf)
+
+    Returns
+    -------
+    size: tuple[int, int]
+        The size of the text in pixels, (width, height)
+
+    """
     draw = ImageDraw.Draw(Image.new('RGB', (1, 1)))
-    _, _, width, height = draw.textbbox((0, 0), text, font)
-    return width, height
+    _, _, *size = draw.textbbox((0, 0), text, font)
+    return size
 
 
-def ascii_to_image(text):
+def ascii_to_image(text: str, font: ImageFont.ImageFont = base_font) -> Image.Image:
+    """
+    Converts the given text to an image using the given font
+
+    Parameters
+    ----------
+    text: str
+        The input text to render
+    font: ImageFont.ImageFont, optional, default: base_font
+        The font to render the text in (default: monos.ttf)
+
+    Returns
+    -------
+    text_image: Image.Image
+        The image of the rendered text
+    """
     text_image = Image.new('RGB', sizeof(text))
     draw = ImageDraw.Draw(text_image)
     draw.text((0, 0), text, (255, 255, 255), font)
     return text_image
 
 
-def get_size_of_char(char):
-    image = ascii_to_image(char)
+def get_brightness_of_char(char: str, font: ImageFont.ImageFont = base_font) -> int:
+    """
+    Gets the brightness of the given character when rendered in the given font
+
+    Parameters
+    ----------
+    char: str
+        The character to find its brightness
+    font: ImageFont.ImageFont, optional, default: base_font
+        The font to use to render the character (default: monos.ttf)
+
+    Returns
+    -------
+    brightness: int
+        The brightness of the character, the number of pixels that are not black
+    """
+    image = ascii_to_image(char, font)
     return (np.array(image) != 0).sum()
 
 
-sorted_letters = sorted(CONVERSION_CHARACTERS, key=get_size_of_char)
+sorted_letters = sorted(CONVERSION_CHARACTERS, key=get_brightness_of_char)
 
 
-def image_to_ascii(image, size=None, charset=None, fix_scaling=True, scale=1, sort_chars=False):
+def image_to_ascii(
+        image: Image | str,
+        size: Optional[tuple[int, int]] = None,
+        charset: Sequence = None,
+        fix_scaling: bool = True,
+        scale: float | tuple[float, float] = 1,
+        sort_chars: bool = False
+) -> str:
+    """
+    Convert image to ASCII art
+
+    Parameters
+    ----------
+    image : Image or str
+        The image to convert to ASCII art, can be given as path or URL or an Image object
+    size : tuple[int, int], optional
+        The final size of the ascii art, if scale is given the scale will be applied upon this size
+    charset : Sequence, optional
+        Characters to use for conversion ordered in lightest to darkest, if not given will use default characters
+    fix_scaling : bool, optional, default True
+        Whether to fix scaling of the image to preserve aspect ratio
+    scale : float or tuple[float, float], optional, default 1
+        Scaling factor of the output image, if tuple is given it will be (width_scale, height_scale)
+    sort_chars : bool, optional, default False
+        If given an unordered charset, it will sort it by the brightness of the characters
+
+    Returns
+    -------
+    output: str
+        The ASCII art representation of the input image
+
+    Raises
+    ------
+    ValueError
+        If image is invalid or cannot be loaded from path or URL
+    """
     if isinstance(image, str):
         try:
             image = Image.open(image)
@@ -43,7 +127,7 @@ def image_to_ascii(image, size=None, charset=None, fix_scaling=True, scale=1, so
         raise ValueError('Invalid image path or URL')
 
     if sort_chars and charset:
-        charset = sorted(charset, key=get_size_of_char)
+        charset = sorted(charset, key=get_brightness_of_char)
     charset = charset or sorted_letters
 
     image_width, image_height = size or image.size
