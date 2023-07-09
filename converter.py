ac4ae77be0ebaa298a1b247462dd3521c537d502
Ajay Ratnam <ajratnam.dev@gmail.com>
2023-07-09 12:25:57 +0530

    Added scaling option and fetching image from url

--- converter.py
+++ converter.py
@@ -1,5 +1,6 @@
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
+from requests import get
 
 from config import CONVERSION_CHARACTERS
 
@@ -27,22 +28,34 @@ def get_size_of_char(char):
 sorted_letters = sorted(CONVERSION_CHARACTERS, key=get_size_of_char)
 
 
-def image_to_ascii(image, size=None, charset=None, fix_scaling=True, sort_chars=False):
+def image_to_ascii(image, size=None, charset=None, fix_scaling=True, scale=1, sort_chars=False):
+    if isinstance(image, str):
+        try:
+            image = Image.open(image)
+        except OSError:
+            try:
+                image = Image.open(get(image, stream=True).raw)
+            except Exception:
+                raise ValueError("Unable to load image from URL")
+        except Exception:
+            raise ValueError("Unable to load image from path")
+    if not isinstance(image, Image.Image):
+        raise ValueError('Invalid image path or URL')
+
     if sort_chars and charset:
         charset = sorted(charset, key=get_size_of_char)
     charset = charset or sorted_letters
-    image_width, image_height = size or image.size
-    image_width *= bool(fix_scaling) + 1
-    scaled_image = image.resize((image_width, image_height)).convert('L')
 
+    image_width, image_height = size or image.size
+
+    if isinstance(scale, int | float):
+        scale = (scale,) * 2
+    image_width = int(image_width * scale[0] * (bool(fix_scaling) + 1))
+    image_height = int(image_height * scale[1])
+
+    scaled_image = image.resize((image_width, image_height)).convert('L')
     image_array = np.array(scaled_image, dtype=int) * len(charset) // 256
     ascii_converted = np.vectorize(charset.__getitem__)(image_array)
 
     output = '\n'.join(map(''.join, ascii_converted))
-    ascii_to_image(output).save('text5.png')
     return output
-
-
-image = Image.open('image.jpg')
-with open('converted.txt', 'w+') as file:
-    file.write(image_to_ascii(image))
