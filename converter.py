caae8ddbfb7c626f5abab3fcf2ffd7f506e83ad6
Ajay Ratnam <ajratnam.dev@gmail.com>
2023-07-09 08:43:34 +0530

    Made function ascii_to_image to view the output as an image

--- converter.py
+++ converter.py
@@ -3,25 +3,38 @@ from PIL import Image, ImageDraw, ImageFont
 
 from config import CONVERSION_CHARACTERS
 
-font = ImageFont.truetype('arial.ttf', 20)
+font = ImageFont.truetype('monos.ttf', 20)
+
+
+def sizeof(text):
+    draw = ImageDraw.Draw(Image.new('RGB', (1, 1)))
+    _, _, width, height = draw.textbbox((0, 0), text, font)
+    return width, height
+
+
+def ascii_to_image(text):
+    text_image = Image.new('RGB', sizeof(text))
+    draw = ImageDraw.Draw(text_image)
+    draw.text((0, 0), text, (255, 255, 255), font)
+    return text_image
 
 
 def get_size_of_char(char):
-    image = Image.new('RGB', (30, 30))
-    draw = ImageDraw.Draw(image)
-    draw.text((0, 0), char, font=font, fill=(255, 255, 255))
+    image = ascii_to_image(char)
     return (np.array(image) != 0).sum()
 
 
 sorted_letters = sorted(CONVERSION_CHARACTERS, key=get_size_of_char)
 
 image = Image.open('image.jpg')
-width, height = image.size
-scaled_image = image.resize((width * 2, height)).convert('L')
+image_width, image_height = image.size
+scaled_image = image.resize((image_width * 2, image_height)).convert('L')
 
 image_array = np.array(scaled_image, dtype=int) * len(sorted_letters) // 256
 ascii_converted = np.vectorize(sorted_letters.__getitem__)(image_array)
 
-with open('ascii.txt', 'w') as f:
-    for row in ascii_converted:
-        f.write(''.join(row) + '\n')
+output = ""
+for row in ascii_converted:
+    output += ''.join(row) + '\n'
+
+ascii_to_image(output).save('text.png')
